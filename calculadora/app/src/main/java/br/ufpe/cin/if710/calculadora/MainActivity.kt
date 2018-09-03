c5c8f22f4bba94bd6681c68659c29e65eef327c8
João Pedro <jprmoisakis@gmail.com>
2018-09-03 00:58:43 -0300

    primeira questão

--- calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
@@ -2,12 +2,95 @@ package br.ufpe.cin.if710.calculadora
 
 import android.app.Activity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Listeners dos botoes, para que ao clicar seja adicionado a formula no campo de texto
+        btn_7.setOnClickListener{
+            text_calc.append("7")
+        }
+        btn_8.setOnClickListener{
+            text_calc.append("8")
+        }
+        btn_9.setOnClickListener{
+            text_calc.append("9")
+        }
+        //metodo append normal nao funciona para caracteres diferentes de numeros e ponto, criei um stringbuilder para que funcionasse
+        // e usei o setSelection para corrigir o problema que ocorre ao usar setText(o cursor do text ficava perdido)
+        btn_Divide.setOnClickListener{
+            val sb = StringBuilder()
+            sb.append(text_calc.text).append("/")
+            text_calc.setText(sb.toString())
+            text_calc.setSelection(text_calc.length())
+        }
+        btn_4.setOnClickListener{
+            text_calc.append("4")
+        }
+        btn_5.setOnClickListener{
+            text_calc.append("5")
+        }
+        btn_6.setOnClickListener{
+            text_calc.append("6")
+        }
+        btn_Multiply.setOnClickListener{
+            val sb = StringBuilder()
+            sb.append(text_calc.text).append("*")
+            text_calc.setText(sb.toString())
+            text_calc.setSelection(text_calc.length())
+        }
+        btn_1.setOnClickListener{
+            text_calc.append("1")
+        }
+        btn_2.setOnClickListener{
+            text_calc.append("2")
+        }
+        btn_3.setOnClickListener{
+            text_calc.append("3")
+        }
+        btn_Subtract.setOnClickListener{
+            val sb = StringBuilder()
+            sb.append(text_calc.text).append("-")
+            text_calc.setText(sb.toString())
+            text_calc.setSelection(text_calc.length())
+        }
+        btn_Dot.setOnClickListener{
+            text_calc.append(".")
+        }
+        btn_0.setOnClickListener{
+            text_calc.append("0")
+        }
+        btn_Equal.setOnClickListener{
+            
+        }
+        btn_Add.setOnClickListener{
+            val sb = StringBuilder()
+            sb.append(text_calc.text).append("+")
+            text_calc.setText(sb.toString())
+            text_calc.setSelection(text_calc.length())
+        }
+        btn_LParen.setOnClickListener{
+            val sb = StringBuilder()
+            sb.append(text_calc.text).append("(")
+            text_calc.setText(sb.toString())
+            text_calc.setSelection(text_calc.length())
+        }
+        btn_Power.setOnClickListener{
+            val sb = StringBuilder()
+            sb.append(text_calc.text).append("^")
+            text_calc.setText(sb.toString())
+            text_calc.setSelection(text_calc.length())
+        }
+        btn_Clear.setOnClickListener{
+            text_calc.setText("")
+        }
+
+
     }
 
     //Como usar a função:
